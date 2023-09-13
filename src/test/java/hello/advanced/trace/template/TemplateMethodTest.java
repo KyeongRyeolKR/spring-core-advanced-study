3389bc9ad56bf5ab9138cf3ae3296862279531bf
김경렬 <rlarudfuf802@naver.com>
2023-09-13 15:01:13 +0900

    템플릿 메서드 패턴 - 예제3

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -50,4 +50,28 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template2.execute();
     }
+
+    /**
+     * 자꾸 새로운 클래스를 만들기는 불편하기 때문에 익명 내부 클래스를 사용해서 편리하게 구현
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1 = {}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2 = {}", template2.getClass());
+        template2.execute();
+    }
 }
