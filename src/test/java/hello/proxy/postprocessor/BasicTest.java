bb6dfc43c40f0c8d3a7539e96ce03431f50f64fa
김경렬 <rlarudfuf802@naver.com>
2023-09-19 19:53:03 +0900

    빈 후처리기 - 예제 코드1

---
+++ src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,53 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 빈 후처리기(bean post processor)
+ * 빈을 생성하고 난 후 등록하기 직전에 객체를 조작하고 싶다면 빈 후처리기를 사용하면 된다.
+ * - 객체를 조작할 수도 있고, 완전히 다른 객체로 변경할 수도 있다.
+ */
+public class BasicTest {
+
+    @Test
+    void basicConfig() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        // A는 빈으로 등록된다.
+        A a = applicationContext.getBean("beanA", A.class);
+        a.helloA();
+
+        // B는 빈으로 등록되지 않는다.
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(B.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+}
